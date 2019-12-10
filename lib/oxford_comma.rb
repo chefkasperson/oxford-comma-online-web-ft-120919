fc7c4dc93f78e9f6ae0c681ac42f6136674ce725
Erik Kasperson <chefkasperson@gmail.com>
2019-12-10 07:37:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,5 +4,11 @@ def oxford_comma(array)
     array.join
   elsif length == 2
     array.join(" and ")
+  elsif length >= 3
+    x = array.last
+    y = array.pop
+    z = y.join(", ")
+    a = z + " and " + x
+    a
   end
 end
